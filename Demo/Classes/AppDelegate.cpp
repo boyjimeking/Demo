fe567a749074680e43cd900a09c15ddd0cc3e4f9
sunny352 <sunny352@gmail.com>
2012-12-27 10:43:45 +0800

    2.1

--- Demo/Classes/AppDelegate.cpp
+++ Demo/Classes/AppDelegate.cpp
@@ -7,7 +7,7 @@
 //
 
 #include "AppDelegate.h"
-
+#include "AppMacros.h"
 #include "cocos2d.h"
 #include "WorldManager.h"
 #include "../CSProtocol/CSPipeline.h"
@@ -25,15 +25,41 @@ AppDelegate::~AppDelegate()
 
 bool AppDelegate::applicationDidFinishLaunching()
 {
-    // initialize director
-    CCDirector *pDirector = CCDirector::sharedDirector();
-    pDirector->setOpenGLView(CCEGLView::sharedOpenGLView());
-	//CCEGLView::sharedOpenGLView()->setFrameSize(960, 640);
-	CCEGLView::sharedOpenGLView()->setDesignResolutionSize(960, 640, kResolutionShowAll);
-
-    // enable High Resource Mode(2x, such as iphone4) and maintains low resource on other devices.
-    // pDirector->enableRetinaDisplay(true);
-
+    CCDirector* pDirector = CCDirector::sharedDirector();
+    CCEGLView* pEGLView = CCEGLView::sharedOpenGLView();
+    
+    pDirector->setOpenGLView(pEGLView);    // enable High Resource Mode(2x, such as iphone4) and maintains low resource on other devices.
+    //pDirector->enableRetinaDisplay(true);
+    designResolutionSize.width=960;
+    designResolutionSize.height=640;
+    pEGLView->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, kResolutionNoBorder);
+    
+	CCSize frameSize = pEGLView->getFrameSize();
+    
+    // In this demo, we select resource according to the frame's height.
+    // If the resource size is different from design resolution size, you need to set contentScaleFactor.
+    // We use the ratio of resource's height to the height of design resolution,
+    // this can make sure that the resource's height could fit for the height of design resolution.
+    
+    // if the frame's height is larger than the height of medium resource size, select large resource.
+	if (frameSize.height > mediumResource.size.height)
+	{
+		CCFileUtils::sharedFileUtils()->setResourceDirectory(largeResource.directory);
+        pDirector->setContentScaleFactor(MIN(largeResource.size.height/designResolutionSize.height, largeResource.size.width/designResolutionSize.width));
+	}
+    // if the frame's height is larger than the height of small resource size, select medium resource.
+    else if (frameSize.height > smallResource.size.height)
+    {
+        CCFileUtils::sharedFileUtils()->setResourceDirectory(mediumResource.directory);
+        pDirector->setContentScaleFactor(MIN(mediumResource.size.height/designResolutionSize.height, mediumResource.size.width/designResolutionSize.width));
+    }
+    // if the frame's height is smaller than the height of medium resource size, select small resource.
+	else
+    {
+		CCFileUtils::sharedFileUtils()->setResourceDirectory(smallResource.directory);
+        pDirector->setContentScaleFactor(MIN(smallResource.size.height/designResolutionSize.height, smallResource.size.width/designResolutionSize.width));
+    }
+    pDirector->setContentScaleFactor(1.0f);
     // turn on display FPS
     pDirector->setDisplayStats(true);
 
