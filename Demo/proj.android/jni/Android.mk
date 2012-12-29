ce0d9a4a6d2f3f71b0932d6c91f9cdc04eb900e9
Sailor_WorkingWin <sunny352@gmail.com>
2012-12-29 16:03:33 +0800

    修正jni的Android.mk配置错误

--- Demo/proj.android/jni/Android.mk
+++ Demo/proj.android/jni/Android.mk
@@ -12,11 +12,11 @@ LOCAL_SRC_FILES := hellocpp/main.cpp \
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes\
 					$(LOCAL_PATH)/../../GameFramework                   
 
-LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static gameframework_static
             
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,CocosDenshion/android) \
 $(call import-module,cocos2dx) \
 $(call import-module,extensions) \
-$(call import-module,GameFramework)
+$(call import-module,../GameFramework)
